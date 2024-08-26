7a425304d3895c85bc5bc02db832dccd5f326c06
jinaa11 <jinaa2047@gmail.com>
2024-08-26 20:15:39 +0900

    learn: k6 basic study - vus, duration options / sleep

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 import http from 'k6/http';
 import {sleep} from 'k6';
 
-// export const options = {
-//   vus: 10,
-//   duration: '30s',
-// };
+// 추가 옵션
+export const options = {
+  vus: 1, // 가상 사용자
+  duration: '10s' // 10초 동안
+};
 
 // 단일 사용자가 "https://test.k6.io"에 대해 요청을 보냄
 // "https://test.k6.io" => k6에서 제공하는 간단한 테스트 웹사이트
@@ -29,6 +30,7 @@ export default function() {
 //   http.post(url, payload, params);
 // }
 
+// html로 결과물 확인 가능
 // export function handleSummary(data) {
 //   return {
 //     "summary.html" : htmlReport(data)
